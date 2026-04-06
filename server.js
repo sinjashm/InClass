956db3cf512c450039fbeab45d7a185aa29b893a
Jashman Singh <sinjashm@sheridancollege.ca>
2026-04-06 17:21:08 -0400

    updated app message

--- server.js
+++ server.js
@@ -2,15 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-res.send('Hello, Dockerized Node.js App!');
+res.send('Hello, Dockerized Node.js App!!');
 });
 app.listen(PORT, () => {
 console.log(`App running on http://localhost:${PORT}`);
 });
 
-//jashman
-//d
-//jashman
-//i
-//kj
-//kass
